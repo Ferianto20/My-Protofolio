95f5f330931b0a6b14134b45f0c6217bb7f31cb5
Ferianto20 <80211248+Ferianto20@users.noreply.github.com>
2021-03-07 22:42:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>My Protofolio (Ferianto)</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <br>
+               <h3 style="color: cornsilk;">My Portofolio</h3>
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.jpg" alt="" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#MyProject">MyProject</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#askme">ask me</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Ferianto
+                        <span class="text-primary">"Ryan"</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Jl Lagoa TRS GG4 B1 No 38 / 0859-0034-0011
+                        <a href="mailto:ryanyrs20@gmail.com">Ryanyrs20@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">My experience is in the field of Designing 3D models, creating Augmented Reality applications, and creating Android-based applications</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Universitas YARSI</h3>
+                        <div class="flex-shrink-0"><span class="text-primary">July 2017 - Now</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SMK TANJUNG PRIOK 1</h3>
+                        <div class="flex-shrink-0"><span class="text-primary">july 2014 - June 2017</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SMP TANJUNG PRIOK</h3>
+                            </div>
+                        <div class="flex-shrink-0"><span class="text-primary">July 2011 - July 2014</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SDN NEGERI 04</h3>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">June 2005 - June 2011</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- MYPROJECT-->
+            <section class="resume-section" id="MyProject">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">MyProject</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <table class="content-table">
+                                <thead>
+                                  <tr>
+                                    <th>No</th>
+                                    <th>Project</th>
+                                    <th>AS</th>
+                                    <th>Description Project</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td>1</td>
+                                    <td>Aplikasi Android Augmented Reality MRT</td>
+                                    <td>Desaigner</td>
+                                    <td>Membuat desain 3D MRT menggunakan BLENDER dan memberikan texture material pada 3D objek MRT</td>
+                                  </tr>
+                                  <tr class="active-row">
+                                    <td>2</td>
+                                    <td>Aplikasi Android The Little Zoo</td>
+                                    <td>Desaigner and coder</td>
+                                    <td>Membuat desain buku belajar diusia dini dan membuat desain UI serta memberikan efek animmasi dan suara</td>
+                                  </tr>
+                                  <tr>
+                                    <td>3</td>
+                                    <td>Aplikasi AR UKM KREASI</td>
+                                    <td>Desaigner and coder</td>
+                                    <td>Membuat ui desain aplikasi dan membuat image target pada Id Card untuk anggota UKM KREASI dan memasukan video tentang KREASI sehingga anggota KREASI dapat melihat tentang KREASI </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="load2">
+                        <h5 style="margin-left: 20px;">Java</h5>
+                        <div class="progress2" style="width:80%">80%</div>
+                    </div>
+                    <br>
+				<div class="load4">
+                    <h5 style="margin-left: 20px;">Blender</h5>
+					<div class="progress4" style="width:50%">50%</div>
+				</div>
+                <br>
+                <div class="load2">
+                    <h5 style="margin-left: 20px;">Unity</h5>
+                    <div class="progress2" style="width:80%">80%</div>
+                </div>
+                <br>
+				<div class="load4">
+                    <h5 style="margin-left: 20px;">Mobile Programming</h5>
+					<div class="progress4" style="width:65%">65%</div>
+				</div>
+                <br>
+                <div class="load2">
+                    <h5 style="margin-left: 20px;">Augmented Reality</h5>
+                    <div class="progress2" style="width:80%">80%</div>
+                </div>
+                <br>
+                
+            </section>
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="askme">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">ask me ?</h2>
+                    <div class = "container-fluid">
+                        <div class="row row-card" style="background-color: coral;">
+                            <div class="col-lg">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <form action = "" methode="POST">
+                                             <label>Nama Lengkap :</label>
+                                            <input type="text" placeholder="Masukkan Nama Anda..." class="form form-control">
+                                            <label>Email :</label>
+                                            <input type="text" placeholder="Masukkan Email Anda..." class="form form-control">
+                                            <label>Masukan :</label>
+                                            <input type="text" placeholder="Masukkan Komentar Anda..." class="form form-control">
+                                            <br>
+                                            <button class="btn btn-success">SUBMIT!</button>
+                                        </form>
+                                    </div>
+                                  </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Awards-->
+            <section class="resume-section" id="awards">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Awards & Certifications</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Perluas kehadiran online Anda dengan strategi bisnis yang tepat
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Webinar Bedah CV dan Tips Menhadapi HRD saat Interview
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            <sup>st</sup>
+                            Webinar Bedah CV dan Tips Menhadapi HRD saat Interview
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            <sup>st</sup>
+                            Sertifikat Keanggotaan UKM KREASI 2019/2020
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
